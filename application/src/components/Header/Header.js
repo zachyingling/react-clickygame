6bb6cc96f0b9b08bf1b2e89a4c5544051e283c86
zachary <zpyingling9559@gmail.com>
2020-05-03 01:07:58 -0400

    initial files and setup of application

---
+++ application/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+  return (
+    <header>
+      <h1><strong>Matching Game</strong></h1>
+      <h1>Time: </h1>
+    </header>
+  );
+}
+
+export default Header;
