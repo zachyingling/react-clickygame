ca300543fba8104e8b00a35d6e3728ed2752166f
zachary <zpyingling9559@gmail.com>
2020-05-04 01:17:29 -0400

    adding container to webpage; adding structured FruitCard component

--- application/src/App.js
+++ application/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
       <Jumbotron/>
+      <Container>
+      
+      </Container>
       <Footer/>
     </div>
   );
---
+++ application/src/components/FruitCard/FruitCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./FruitCard.css";
+
+function FruitCard (props) {}
+
+export default FruitCard
