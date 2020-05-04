3dea3601ca717252a0188513d577f61e180f4c31
zachary <zpyingling9559@gmail.com>
2020-05-04 01:03:45 -0400

    adding footer to the application

--- application/src/App.js
+++ application/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import Jumbotron from "./components/Jumbotron/Jumbotron"
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
       <Jumbotron/>
+      <Footer/>
     </div>
   );
 }
---
+++ application/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer () {
+  return(
+    <footer>
+      <h1>Matching Game</h1>
+      <a href="https://github.com/zachyingling/react-clickygame">Repo</a>
+    </footer>
+  );
+}
+
+export default Footer;
