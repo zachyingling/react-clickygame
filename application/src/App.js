99197dfa250f2f3c72ec788cfa5a56600f99d0ec
zachary <zpyingling9559@gmail.com>
2020-05-04 20:08:38 -0400

    turning app.js into a class

--- application/src/App.js
+++ application/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Footer from "./components/Footer/Footer";
 import Container from "./components/Container/Container";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header/>
-      <Jumbotron/>
-      <Container>
-      
-      </Container>
-      <Footer/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Header/>
+        <Jumbotron/>
+        <Container>
+        
+        </Container>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
