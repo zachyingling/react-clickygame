a30bd20fd73cbde584d25006c0e1f2409666e913
Zach Yingling <zpyingling9559@gmail.com>
2020-07-13 22:11:12 -0400

    changing fruitcard component to a class component to get the flip package to work with the card component

--- src/components/FruitCard/FruitCard.js
+++ src/components/FruitCard/FruitCard.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./FruitCard.css";
+import ReactCardFlip from "react-card-flip";
 
-function FruitCard (props) {
-  return (
-    <div className="card" id="cardMain" onClick={() => props.handleSelected(props.name, props.id)}>
-      <div className="img-container">
-        <img alt={props.name} src={process.env.PUBLIC_URL + "/" + props.image} height="150" width="170"/>
-      </div>
-    </div>
-  );
+class FruitCard extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      isFlipped: false
+    };
+  }
+
+  render(){
+    return (
+        <div className="card" id="cardMain" onClick={() => this.props.handleSelected(this.props.name, this.props.id)}>
+          <div className="img-container">
+            <img alt={this.props.name} src={process.env.PUBLIC_URL + "/" + this.props.image} height="150" width="170"/>
+          </div>
+        </div>
+      );
+    }
 }
 
 export default FruitCard;
