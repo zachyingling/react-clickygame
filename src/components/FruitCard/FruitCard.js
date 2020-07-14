ffca0697e7a22a4349e04e9c0f54b4fcce7c5825
Zach Yingling <zpyingling9559@gmail.com>
2020-07-13 22:40:22 -0400

    getting the flip to work but still need to get the unselect optioon to select to work and the other image to be on the face of the card

--- src/components/FruitCard/FruitCard.js
+++ src/components/FruitCard/FruitCard.js
@@ -8,15 +8,28 @@ class FruitCard extends React.Component {
     this.state = {
       isFlipped: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
   }
 
   render(){
     return (
-        <div className="card" id="cardMain" onClick={() => this.props.handleSelected(this.props.name, this.props.id)}>
-          <div className="img-container">
-            <img alt={this.props.name} src={process.env.PUBLIC_URL + "/" + this.props.image} height="150" width="170"/>
+        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+          <div className="card" id="cardMain" onClick={() => { this.props.handleSelected(this.props.name, this.props.id); this.handleClick();}}>
+            <div className="img-container">
+              <img alt={this.props.name} src={process.env.PUBLIC_URL + "/" + this.props.image} height="150" width="170"/>
+            </div>
           </div>
-        </div>
+
+          <div className="card" id="cardMain" onClick={() => { this.props.handleSelected(this.props.name, this.props.id); this.handleClick();}}>
+            <div className="img-container">
+              <img alt={this.props.name} src={process.env.PUBLIC_URL + "/" + this.props.image} height="150" width="170"/>
+            </div>
+          </div>
+        </ReactCardFlip>
       );
     }
 }
